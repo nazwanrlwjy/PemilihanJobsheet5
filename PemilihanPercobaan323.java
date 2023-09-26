fa3f3c51045d808df4d6c703c56c5d1377e5b008
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-09-26 16:23:33 +0700

    Add files via upload

---
+++ PemilihanPercobaan323.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PemilihanPercobaan323 {
+    public static void main(String[] args){
+       
+        double angka1, angka2, hasil;
+        char operator;
+
+        Scanner input23 = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input23.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input23.nextDouble();
+        System.out.print("Masukkan operator (+ - * /) ");
+        operator = input23.next().charAt(0);
+
+        switch (operator) {
+        case '+':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + " + " + angka2 + "=" + hasil);break;
+        case '-':
+        hasil = angka1 - angka2;
+        System.out.println(angka1 + " - " + angka2 + "=" + hasil);break;
+        case '*':
+        hasil = angka1 * angka2;
+        System.out.println(angka1 + " * " + angka2 + "=" + hasil);break;
+        case '/':
+        hasil = angka1 / angka2;
+        System.out.println(angka1 + "/" + angka2 + "=" + hasil);break;
+    }
+    }
+}
